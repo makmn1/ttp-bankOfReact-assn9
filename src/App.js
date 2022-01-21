635ed30e7f5d9bd01d686ac18cd6747c2384e9ae
Aseel Alshohatee <46002084+Alshohatee@users.noreply.github.com>
2022-01-20 20:07:26 -0500

    add the user profile component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react'
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home'
-
+import UserProfile from './components/UserProfile'
 export default function App() {
 
-    const [accountBalance, setAccountBalance] = React.useState(14568.27)
+    const [accountBalance, setAccountBalance] = React.useState( 14568.27 )
+    const [currentUser, setCurrentUsere] = React.useState( {
+        userName: 'bob_loblaw',
+        memberSince: '08/23/99',
+    } )
 
     return (
-    <BrowserRouter>
-        <Routes>
-            <Route exact path ="/" element={<Home accountBalance={accountBalance}/>} />
-        </Routes>
-    </BrowserRouter>
-  );
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<Home accountBalance={accountBalance} />} />
+                <Route exact path="/userProfile" element={<UserProfile userName={currentUser.userName} memberSince={currentUser.memberSince} />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 
